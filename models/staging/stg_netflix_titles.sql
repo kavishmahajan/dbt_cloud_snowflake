2a5ebd564bde01ab01f07f01a48c52b7af35e866
Kavish Mahajan <kavish.mahajan2017@gmail.com>
2026-02-22 15:44:06 +0000

    initial merge

---
+++ models/staging/stg_netflix_titles.sql
@@ -0,0 +1,16 @@
+{{ config(schema='ANALYTICS') }}
+
+select
+    show_id,
+    type,
+    title,
+    director,
+    'cast' as cast_members,
+    country,
+    date_added,
+    release_year,
+    rating,
+    duration,
+    listed_in,
+    description
+from {{ source("netflix_raw", "NETFLIX_TITLES") }}
